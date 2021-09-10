1e2ce6e37eaafa154c218bab666396aab368a808
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-09-10 00:10:35 -0400

    find work

--- api/server.js
+++ api/server.js
@@ -4,10 +4,14 @@ const server = express();
 
 server.use(express.json());
 server.use("/api/recipe", recipeRouter);
-
+server.use("/", (req, res) => {
+  res.send(`<h1>Hey! I love code but hate this =)</h1>
+  `);
+});
 server.use((err, req, res, next) => {
   // eslint-disable-line
   res.status(500).json({
+    custom: "Something went wrong with the router",
     message: err.message,
     stack: err.stack,
   });
